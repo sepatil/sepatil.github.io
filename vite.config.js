bf5403ca7df3da3c9f86eb3dd90d2034ebde92a7
sepatil <sepatil0612@gmail.com>
2025-12-17 21:11:34 +0530

    style path change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,21 @@ const path = require("path");
 
 export default {
   root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/js/theme.js",
+        chunkFileNames: "assets/js/[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "assets/css/style.css"; // fixed CSS filename
+          }
+          return "assets/[name][extname]"; // default for others
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
